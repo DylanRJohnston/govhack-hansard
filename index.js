accc46da79d69fa18ddf4b41a6e9d9fed93799a3
Dylan R. Johnston <Dylan.R.Johnston@gmail.com>
2015-07-05 11:32:28 +0800

    Added CORS

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ var app     = express();
 
 app.get('/search',
     function(req, res) {
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+         res.header('Access-Control-Allow-Headers', 'Content-Type');
+
         var startDate = moment(req.query.startDate, "DD/MM/YYYY");
         var endDate   = moment(req.query.endDate,    "DD/MM/YYYY");
 
