e765ed9bf6081dc83dfd755b375bb2506297884f
Dylan R. Johnston <Dylan.R.Johnston@gmail.com>
2015-07-05 10:30:21 +0800

    Ain't no body got time for commit messages.

---
+++ rest-api.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var db      = require('monk')('localhost/hansdb');
+var moment  = require('moment');
+
+
+var sessions = db.get('sessions');
+
+var app     = express();
+
+app.get('/search',
+    function(req, res) {
+        var startDate = moment(req.query.startDate, "DD/MM/YYYY");
+        var endDate   = moment(req.query.endDate,    "DD/MM/YYYY");
+
+        console.log("startDate " + startDate.toDate())
+        console.log("endDate " + endDate.toDate())
+
+        var query = {
+            date: {
+                    $gt: startDate.toDate(),
+                    $lt: endDate.toDate()
+            }
+        };
+
+        sessions.find(query, function (error, docs){
+            if (error) console.log(error);
+
+            res.json(docs);
+        });
+    }
+);
+
+var server = app.listen(8000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
--- scrapeTranscripts.js
+++ scrapeTranscripts.js
@@ -7,20 +7,21 @@ var db      = require('monk')('localhost/hansdb');
 var ProgressBar = require('progress');
 
 var sessions = db.get('sessions');
-sessions.index('session_header.date session_header.chamber', {unique: true});
+//sessions.index('session_header.date session_header.chamber', {unique: true});
 
 
-var parser = new xml2js.Parser({
-    explicitArray : false,
-    attrkey: 'attr',
-    tagNameProcessors: [
-        function(name) {return name.replace(/\./g, '_')}
-    ]
-});
+// var parser = new xml2js.Parser({
+//     explicitArray : false,
+//     attrkey: 'attr',
+//     tagNameProcessors: [
+//         function(name) {return name.replace(/\./g, '_')}
+//     ]
+// });
 
 var bar = new ProgressBar('Pulling [:bar] :current/:total :percent :eta', {total: 0});
 
 var hansardQueue = async.queue(function(task, callback) {
+    console.log(task);
     request(task, function(error, response, body) {
             if (error) {
                 console.log("Error in hansard request " + error);
@@ -28,24 +29,53 @@ var hansardQueue = async.queue(function(task, callback) {
                 return;
             }
 
-            parser.parseString(body,
-                function(error, result) {
-                    if (error) {
-                        console.log("Error in parsing hansard XML " + error);
-                        callback(error);
-                        return;
-                    }
+            var sanatisedBody = body.replace(/(<\/?[a-z]+)\./g, "$1_");
+            var $ = cheerio.load(sanatisedBody);
 
-                    sessions.insert(result.hansard, function(error, doc) {
-                        bar.tick(1);
-                        if (error) console.log("Error in mongodb insert " + error);
-                        callback(error);
+            var date = moment($('session_header > date').text(), "YYYY-MM-DD").toDate();
+
+            //subdebate_1 represents all discussions on an issue
+            $('subdebate_1').each(function(){
+                var title = $('> subdebateinfo > title', this).text();
+
+                $('speech', this).each(function() {
+                    var talkers = {};
+                    $('talker', this).each(function() {
+                        var id = $('> name_id', this).text();
+                        if (talkers[id]) return;
+
+                        talkers[id] = {};
+                        talkers[id].name = $('> name', this).text();
+                        talkers[id].electorate = $('> electorate', this).text();
+                        talkers[id].party = $('> party', this).text();
                     });
+
+                    var recentSpeaker = "";
+                    $('span.HPS-Normal', this).each(function() {
+                        var speaker = $('a', this).attr("href");
+                        var interjecting = $('[class$="Interjecting"]', this).count != 0;
+
+                        if (speaker && talkers[speaker]) {
+                            recentSpeaker = speaker;
+                        }
+
+                        sessions.insert({
+                            speaker: talkers[recentSpeaker],
+                            date: date,
+                            bill: title,
+                            interjecting: interjecting,
+                            text: $(this).text()
+                        }, function(error, doc) {
+                            bar.tick(1);
+                            if (error) console.log("Error in mongodb insert " + error);
+                        });
+                    });
+                });
             });
         }
     );
 }, 20);
-hansardQueue.drain = function() {console.log("All hansard pages parsed"); db.close();}
+hansardQueue.drain = function() {console.log("All hansard pages parsed"); /*db.close();*/}
 
 var indexQueue = async.queue(function(task, callback) {
     request(
